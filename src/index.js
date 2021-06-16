86534ded86a1682a5415e0d0deae5215e8704744
Manvi <msinghal3102@gmail.com>
2021-06-16 17:09:31 +0530

    Added different books

--- src/index.js
+++ src/index.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
+const books=[
+  {
+    id:1,
+  img: 'https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY327_FMwebp_QL65_.jpg' ,
+  title: 'I Love You to the Moon and Back',
+  author: 'Amelia Hepworth',
+  },
+  {
+    id:2,
+    img:'https://m.media-amazon.com/images/I/91NBDxB0JyL._AC_UY327_FMwebp_QL65_.jpg',
+    title:'Our Class is a Family',
+    author:' Shannon Olsen',
+  },
+  {
+    id:3,
+    img:'https://m.media-amazon.com/images/I/61n9L8w08sL._AC_UY327_FMwebp_QL65_.jpg',
+    title:'The Vanishing Half: Sunday Times Bestseller',
+    author:' Brit Bennett',
+  },
+];
 function BookList(){
   return (
   <section className="booklist">
-    <Book/>
-    <Book/>
-    <Book/>
+    {books.map((book)=>{
+       return(
+         <Book key={book.id} book={book}></Book>
+       );
+    })}
+    
     
   </section>
   );
 }
-const Book = ()=>{
+const Book = (props)=>{
+  const{ img, title, author} = props.book;
   return <article className="book">
-    <img src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY327_FMwebp_QL65_.jpg" alt=""/>
-    <h1>I Love You to the Moon and Back</h1>
-    <h4 style={{color:'#617d98', fontSize: '0.75rem', marginTop: '0.25rem'}}>Amelia Hepworth</h4>
+    <img src={img} alt=""/>
+    <h1>{title}</h1>
+    <h4>{author}</h4>
     </article>
 }
 
