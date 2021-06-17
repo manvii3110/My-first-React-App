833364208a27a115d09fc8258a864b5bea157c25
Manvi <msinghal3102@gmail.com>
2021-06-17 14:24:41 +0530

    Completely added all books

---
+++ src/books.js
@@ -0,0 +1,56 @@
+export const books = [{
+        id: 1,
+        img: 'https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY327_FMwebp_QL65_.jpg',
+        title: 'I Love You to the Moon and Back',
+        author: 'Amelia Hepworth',
+    },
+    {
+        id: 2,
+        img: 'https://m.media-amazon.com/images/I/91NBDxB0JyL._AC_UY327_FMwebp_QL65_.jpg',
+        title: 'Our Class is a Family',
+        author: ' Shannon Olsen',
+    },
+    {
+        id: 3,
+        img: 'https://m.media-amazon.com/images/I/61n9L8w08sL._AC_UY327_FMwebp_QL65_.jpg',
+        title: 'The Vanishing Half: Sunday Bestseller',
+        author: ' Brit Bennett',
+    },
+    {
+        id: 4,
+        img: 'https://m.media-amazon.com/images/I/51UA4cTPAhL._AC_UY327_FMwebp_QL65_.jpg',
+        title: 'Murder on the Orient Express',
+        author: ' Agatha Christie',
+    },
+    {
+        id: 5,
+        img: 'https://m.media-amazon.com/images/I/71Q0+4VG1YL._AC_UY327_FMwebp_QL65_.jpg',
+        title: 'Gone Girl',
+        author: ' Gillian Flynn ',
+    },
+    {
+        id: 6,
+        img: 'https://m.media-amazon.com/images/I/81h-YuwLOuL._AC_UY327_FMwebp_QL65_.jpg',
+        title: 'A to Z Mysteries: The Invisible Island ',
+        author: 'Ron Roy',
+    },
+    {
+        id: 7,
+        img: 'https://m.media-amazon.com/images/I/71ErsKpwmrS._AC_UY327_FMwebp_QL65_.jpg',
+        title: 'The Girl on the Train',
+        author: ' Agatha Christie',
+    },
+    {
+        id: 8,
+        img: 'https://m.media-amazon.com/images/I/715cL4W9+SL._AC_UY327_FMwebp_QL65_.jpg',
+        title: 'The Mystery of the Hidden Lab',
+        author: 'Amit Puri',
+    },
+    {
+        id: 9,
+        img: 'https://m.media-amazon.com/images/I/61Gzy4y2gLL._AC_UY327_FMwebp_QL65_.jpg',
+        title: 'The Perfect Murder',
+        author: 'Ruskin Bond',
+    },
+
+];
--- src/index.js
+++ src/index.js
@@ -1,46 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-const books=[
-  {
-    id:1,
-  img: 'https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY327_FMwebp_QL65_.jpg' ,
-  title: 'I Love You to the Moon and Back',
-  author: 'Amelia Hepworth',
-  },
-  {
-    id:2,
-    img:'https://m.media-amazon.com/images/I/91NBDxB0JyL._AC_UY327_FMwebp_QL65_.jpg',
-    title:'Our Class is a Family',
-    author:' Shannon Olsen',
-  },
-  {
-    id:3,
-    img:'https://m.media-amazon.com/images/I/61n9L8w08sL._AC_UY327_FMwebp_QL65_.jpg',
-    title:'The Vanishing Half: Sunday Times Bestseller',
-    author:' Brit Bennett',
-  },
-];
+import {books} from './books'
+import SpecificBook from './Book'
+
+
 function BookList(){
   return (
   <section className="booklist">
     {books.map((book)=>{
        return(
-         <Book key={book.id} book={book}></Book>
+         <SpecificBook key={book.id} {...book}></SpecificBook>
        );
     })}
     
     
   </section>
   );
-}
-const Book = (props)=>{
-  const{ img, title, author} = props.book;
-  return <article className="book">
-    <img src={img} alt=""/>
-    <h1>{title}</h1>
-    <h4>{author}</h4>
-    </article>
 }
 
 
